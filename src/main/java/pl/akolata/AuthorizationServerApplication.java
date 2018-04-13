ea0e48e168b1a53041970cf7357a2a8cbb2de192
a.kolata <olek.tbg.konta@gmail>
2018-04-13 17:59:42 +0200

    Project created

---
+++ src/main/java/pl/akolata/AuthorizationServerApplication.java
@@ -0,0 +1,10 @@
+package pl.akolata;
+
+import org.springframework.boot.SpringApplication;
+
+public class AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+}
